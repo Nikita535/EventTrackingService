c43765bf4c43e0b0e06b44be34ba284e5b07af28
Nikita <nzhigulevskiy@bk.ru>
2023-05-08 12:47:45 +0300

    added the scheduled task sending message about neare event

--- src/main/java/rtuit/lab/Repositories/RegistrationRepository.java
+++ src/main/java/rtuit/lab/Repositories/RegistrationRepository.java
@@ -3,10 +3,12 @@ package rtuit.lab.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rtuit.lab.Models.Registration;
+import rtuit.lab.Models.User;
 
 import java.util.List;
 
 @Repository
 public interface RegistrationRepository extends JpaRepository<Registration,Long> {
     List<Registration> findAllByEvent_Tag(String tag);
+    List<Registration> findAllByUser(User user);
 }
---
+++ src/main/java/rtuit/lab/Schedulers/Scheduler.java
@@ -0,0 +1,45 @@
+package rtuit.lab.Schedulers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import rtuit.lab.Models.Event;
+import rtuit.lab.Models.Registration;
+import rtuit.lab.Models.Role;
+import rtuit.lab.Models.User;
+import rtuit.lab.Repositories.RegistrationRepository;
+import rtuit.lab.Repositories.UserRepository;
+import rtuit.lab.Services.EmailService;
+
+import javax.mail.MessagingException;
+import java.util.List;
+
+
+@Component
+public class Scheduler {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RegistrationRepository registrationRepository;
+
+    @Autowired
+    EmailService emailService;
+
+    @Scheduled(fixedDelay = 172800000)
+    public void sendMessageAboutNearEvent() throws MessagingException {
+        List<User> allUsualUsers = userRepository.findAll().stream()
+                .filter(user -> user.getAuthorities().contains(Role.ROLE_USER)).toList();
+        for(User user : allUsualUsers){
+            List<Event> userEvents = registrationRepository.findAllByUser(user).stream().map(Registration::getEvent).toList();
+            for (Event userEvent : userEvents) {
+                String message = "Привет, " + user.getUsername() + "!" +
+                        " Это напоминание о том, что ты зарегистрирован на событие - " + userEvent.getTitle();
+                emailService.sendSimpleMessage(user.getEmail(), message);
+            }
+
+        }
+    }
+
+}
